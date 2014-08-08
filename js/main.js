b2e85e16b4574fd316a270a6475ae58cf259464e
Luis Gago <luisgago@lagc-peru.com>
2014-08-08 16:35:00 -0500

    5 día

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ $(document).ready( function(){
 	var $countdown = $('.countdown');
     $countdown.scountdown({
     	// timestampgenerator.com
-        timestamp: parseInt(1407463200000, 10),
+        timestamp: parseInt(1407549600000, 10),
         callback: function (d, h, m, s) {
             $(".dias", $countdown).text(d);
             $(".horas", $countdown).text(h);
